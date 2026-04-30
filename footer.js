9bc30500133d7e6a4a298d2cb3fc09a383b19c4d
Jashan337 <131540619+Jashan337@users.noreply.github.com>
2026-04-30 13:31:33 +0700

    update footer element

--- footer.js
+++ footer.js
@@ -1,6 +1,8 @@
 class Footer extends HTMLElement {
   connectedCallback() {
-    this.innerHTML = `<footer>My footer</footer>`;
+    this.innerHTML = `<footer class="py-12 text-center text-neutral-500 text-xs font-medium border-t border-neutral-800 tracking-widest uppercase">
+                     © 2026 NTP Cuber Academy
+                     </footer>`;
   }
 }
 cutomElements.define('my-footer', Footer);
